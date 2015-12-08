2be9d18b0499efe7fded26352f6fcc16bc6b05e6
Steffen685 <steffengriesch@gmail.com>
2015-12-08 14:11:08 +0100

    add code

--- test.py
+++ test.py
@@ -2,23 +2,20 @@ from Bio import SeqIO
 from feature import *
 
 ## Read in the fucking files..
-records = list(SeqIO.parse("vectors-100.gb", "genbank"))
+records = list(SeqIO.parse("vectors.gb", "genbank"))
+primerBindingSites = list(SeqIO.parse("common_primer.mfasta", "fasta"))
 specialTranslatedFeatures = list(SeqIO.parse("tags_epitopes.mfasta", "fasta"))
-primerBindingSites = list (SeqIO.parse("common_primer.mfasta", "fasta"))
 
 
+for i in range (736, 786):
+    recordsSeqToCheckFiveEnd = str(records[i].seq)[0:14]
+    recordsSeqToCheckThreeEnd = (str(records[i].seq)[::-1])[0:14]
 
-## test output
-print("vector seq..")
-for i in range(0,5):
-     print records[i].seq
+    for x in range (0, len(primerBindingSites)):
+        if recordsSeqToCheckFiveEnd == str(primerBindingSites[x].seq)[0:14]:
+            print "Primer Binding Sites: Match at five ' end"
+            print ("Record ID: " + records[i].id)
 
-print("")
-print("PBS seq..")
-for i in range (0,5):
-    print primerBindingSites[i].seq
-
-print("")
-print("STF seq..")
-for i in range(0,5):
-    print specialTranslatedFeatures[i].seq
+        if recordsSeqToCheckThreeEnd == str(primerBindingSites[x].seq)[0:14]:
+            print "Primer Binding Sites: Match at three ' end"
+            print ("Record ID: " + records[i].id)
