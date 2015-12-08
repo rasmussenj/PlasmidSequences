c8eb4848cc10c3dfd5f787d337b7c624b8be3273
Steffen685 <steffengriesch@gmail.com>
2015-12-08 14:30:11 +0100

    add code

--- test.py
+++ test.py
@@ -6,16 +6,26 @@ records = list(SeqIO.parse("vectors.gb", "genbank"))
 primerBindingSites = list(SeqIO.parse("common_primer.mfasta", "fasta"))
 specialTranslatedFeatures = list(SeqIO.parse("tags_epitopes.mfasta", "fasta"))
 
-
-for i in range (736, 786):
+for i in range (730, 780):
     recordsSeqToCheckFiveEnd = str(records[i].seq)[0:14]
     recordsSeqToCheckThreeEnd = (str(records[i].seq)[::-1])[0:14]
 
+    complementRecordsSeqToCheckFiveEnd = str(records[i].seq.complement())[0:14]
+    complementRecordsSeqToCheckThreeEnd = (str(records[i].seq.complement())[::-1])[0:14]
+
     for x in range (0, len(primerBindingSites)):
         if recordsSeqToCheckFiveEnd == str(primerBindingSites[x].seq)[0:14]:
-            print "Primer Binding Sites: Match at five ' end"
+            print "Primer Binding Sites: Match at five' end"
             print ("Record ID: " + records[i].id)
 
         if recordsSeqToCheckThreeEnd == str(primerBindingSites[x].seq)[0:14]:
-            print "Primer Binding Sites: Match at three ' end"
+            print "Primer Binding Sites: Match at three' end"
+            print ("Record ID: " + records[i].id)
+
+        if complementRecordsSeqToCheckFiveEnd == str(primerBindingSites[x].seq)[0:14]:
+            print "Primer Binding Sites: Match at COMPLEMENT five' end"
+            print ("Record ID: " + records[i].id)
+
+        if complementRecordsSeqToCheckThreeEnd == str(primerBindingSites[x].seq)[0:14]:
+            print "Primer Binding Sites: Match at COMPLEMENT TREE' end"
             print ("Record ID: " + records[i].id)
