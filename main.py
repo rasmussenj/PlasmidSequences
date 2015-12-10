5266a7e7ccf379f22a003a064f5be46c5a918172
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2015-12-10 20:45:48 +0100

    omg miar sind scho fascht fertig :-P

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
 
     ## nach dem Statistic ausgefuehrt wurde, beinhaltet der container nur noch
     #  features die oeffter als 10% vorkommen und mind. 3 mal vorkommen
-
-
-    print ("\n\n\n\n\nnur zum zeigen wie ihr zugreiffen koennt\n\n\n\n\n")
-    for feature in feature_container:
-        print(feature.name)
-        # jedes feature kann verschiedene variationen haben
-        for variation_f in feature.varationList:
-            print("| %-10d %10d%% %-10s "% (variation_f.count,  variation_f.present_in_percent, variation_f.seq))
+    #
+    #
+    # print ("\n\n\n\n\nnur zum zeigen wie ihr zugreiffen koennt\n\n\n\n\n")
+    # for feature in feature_container:
+    #     print(feature.name)
+    #     # jedes feature kann verschiedene variationen haben
+    #     for variation_f in feature.varationList:
+    #         print("| %-10d %10d%% %-10s "% (variation_f.count,  variation_f.present_in_percent, variation_f.seq))
 
 
 
@@ -32,10 +32,8 @@ for feature in feature_container:
     for variation_f in feature.varationList:
 
         featureSeq = str(variation_f.seq)
-        if featureSeq != "G":
-
-            matchStartPosition = str(SeqUtils.nt_search(strRecord, featureSeq))
-            print(feature.name)
-            print textwrap.fill("Matching at position: " + matchStartPosition + "...", 100)
+        matchStartPosition = str(SeqUtils.nt_search(strRecord, featureSeq))
+        print(feature.name)
+        print textwrap.fill("Matching at position: " + matchStartPosition + "...", 100)
 
 
