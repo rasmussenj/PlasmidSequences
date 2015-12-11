c4e0ab7fdc7d8db396a1d98914ecc03b626f6334
Steffen685 <steffengriesch@gmail.com>
2015-12-11 14:41:37 +0100

    Ausgabe bearbeitet

--- main.py
+++ main.py
@@ -12,23 +12,12 @@ if __name__ == "__main__":
     feature_container = countFeatures(getFeature(),features_count)
     feature_container = Statistic(feature_container).featureContainer
 
-    ## nach dem Statistic ausgefuehrt wurde, beinhaltet der container nur noch
-    #  features die oeffter als 10% vorkommen und mind. 3 mal vorkommen
-    #
-    #
-    # print ("\n\n\n\n\nnur zum zeigen wie ihr zugreiffen koennt\n\n\n\n\n")
-    # for feature in feature_container:
-    #     print(feature.name)
-    #     # jedes feature kann verschiedene variationen haben
-    #     for variation_f in feature.varationList:
-    #         print("| %-10d %10d%% %-10s "% (variation_f.count,  variation_f.present_in_percent, variation_f.seq))
-
-
 
 record = SeqIO.read("EcoliK12.gb", "genbank")
 strRecord = str(record.seq)
 print strRecord[0:10]
 
+print "%-20s %-20s"% ("FEATURES", "Location/Qualifiers")
 for feature in feature_container:
     for variation_f in feature.varationList:
 
@@ -39,19 +28,31 @@ for feature in feature_container:
         occurrenceComplement = SeqUtils.nt_search(strRecord, featureSeqComplement)
 
         if (len(occurrence) > 1):
-            # print featureSeq[0:10]
-            # print(feature.name)
-            # print "it occur ", (len(occurrence)-1), "times on the forward strand"
-
             for i in range(1,len(occurrence)-1):
-                 print str(feature.name) + ": From:", occurrence[i], "To:", (occurrence[i]+len(featureSeq))
+                 print "%-20s %-20s"% (str(feature.name), (str(occurrence[i]) + ".." + str((occurrence[i]+len(featureSeq)))))
+                 if variation_f.product != None:
+                     print "%-20s %-20s"% ("", str("/product=\"" + variation_f.product[0] + "\""))
+                 if variation_f.gene != None:
+                     print "%-20s %-20s"% ("", str("/gene=\"" + variation_f.gene[0] + "\""))
+                 if variation_f.bound_moiety != None:
+                     print "%-20s %-20s"% ("", str("/bound_moiety=\"" + variation_f.bound_moiety[0] + "\""))
+                 if variation_f.mobile != None:
+                     print "%-20s %-20s"% ("", str("/mobile=\"" + variation_f.mobile[0] + "\""))
+                 if variation_f.note != None:
+                     print "%-20s %-20s"% ("", str("/note=\"" + variation_f.note[0] + "\""))
+
 
         if (len(occurrenceComplement) > 1):
-            # print featureSeqComplement[0:10]
-            # print(feature.name)
-            # print "it occur ", (len(occurrenceComplement)-1), "times on the complement strand"
-
             for i in range(1, len(occurrenceComplement)-1):
-                 print str(feature.name) + " In complement From:", occurrenceComplement[i], "To:", (occurrenceComplement[i]+len(featureSeqComplement))
-
+                 print "%-20s %-20s"% (str(feature.name), ("complement(" + (str(occurrenceComplement[i]) + ".." + str((occurrenceComplement[i]+len(featureSeqComplement)))) + ")"))
+                 if variation_f.product != None:
+                     print "%-20s %-20s"% ("", str("/product=\"" + variation_f.product[0] + "\""))
+                 if variation_f.gene != None:
+                     print "%-20s %-20s"% ("", str("/gene=\"" + variation_f.gene[0] + "\""))
+                 if variation_f.bound_moiety != None:
+                     print "%-20s %-20s"% ("", str("/bound_moiety=\"" + variation_f.bound_moiety[0] + "\""))
+                 if variation_f.mobile != None:
+                     print "%-20s %-20s"% ("", str("/mobile=\"" + variation_f.mobile[0] + "\""))
+                 if variation_f.note != None:
+                     print "%-20s %-20s"% ("", str("/note=\"" + variation_f.note[0] + "\""))
 
