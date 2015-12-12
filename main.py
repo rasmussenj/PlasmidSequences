8d30c87f5672e08434886e8d3bab519580183a74
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2015-12-12 11:02:37 +0100

    omg miar sind scho fascht fertig :-P

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ if __name__ == "__main__":
     newRecord.id=record.id
     newRecord.name = record.name
     newRecord.description = record.description
+    # newRecord.keywords = record.keywords
+    # newRecord.source = record.source
+    # newRecord.organism = record.organism
+    # newRecord.authors = record.authors
 
     strRecord = str(record.seq)
 
@@ -33,7 +37,6 @@ if __name__ == "__main__":
             featureSeqComplement = str(variation_f.seq.complement())
 
             occurrence = SeqUtils.nt_search(strRecord, featureSeq)
-            #occurrenceComplement = SeqUtils.nt_search(strRecord, featureSeqComplement)
 
             if (len(occurrence) > 1):
                 for i in range(1,len(occurrence)-1):
@@ -50,9 +53,21 @@ if __name__ == "__main__":
                         newFeature.qualifiers['note'] = variation_f.note
                     newRecord.features.append(newFeature)
 
-
+            # occurrenceComplement = SeqUtils.nt_search(strRecord, featureSeqComplement)
             # if (len(occurrenceComplement) > 1):
             #     for i in range(1, len(occurrenceComplement)-1):
+            #         newFeature = SeqFeature(FeatureLocation(occurrence[i],occurrence[i]+len(featureSeq), strand=-1), type=str(feature.name))
+            #         if variation_f.product != None:
+            #             newFeature.qualifiers['product'] = variation_f.product
+            #         if variation_f.gene != None:
+            #             newFeature.qualifiers['gene'] = variation_f.gene
+            #         if variation_f.bound_moiety != None:
+            #             newFeature.qualifiers['bound_moiety'] = variation_f.bound_moiety
+            #         if variation_f.mobile != None:
+            #             newFeature.qualifiers['mobile'] = variation_f.mobile
+            #         if variation_f.note != None:
+            #             newFeature.qualifiers['note'] = variation_f.note
+            #         newRecord.features.append(newFeature)
             #          print "%-20s %-20s"% (str(feature.name), ("complement(" + (str(occurrenceComplement[i]) + ".." + str((occurrenceComplement[i]+len(featureSeqComplement)))) + ")"))
             #          if variation_f.product != None:
             #              print "%-20s %-20s"% ("", str("/product=\"" + variation_f.product[0] + "\""))
@@ -65,6 +80,6 @@ if __name__ == "__main__":
             #          if variation_f.note != None:
             #              print "%-20s %-20s"% ("", str("/note=\"" + variation_f.note[0] + "\""))
 
-    output_handle = open("example.gb", "w")
+    output_handle = open("outbput.gb", "w")
     SeqIO.write(newRecord, output_handle, "genbank")
     output_handle.close()
