515bb0401af87c05544a5fa2de838934597b09e0
Steffen685 <steffengriesch@gmail.com>
2015-12-13 15:25:49 +0100

    add some fucking shit

--- test.py
+++ test.py
@@ -1,26 +1,42 @@
 from Bio import SeqIO
+from Bio import SeqUtils
 from Bio.SeqUtils import six_frame_translations
 
 records = list(SeqIO.parse("vectors.gb", "genbank"))
 primerBindingSites = list(SeqIO.parse("common_primer.mfasta", "fasta"))
 specialTranslatedFeatures = list(SeqIO.parse("tags_epitopes.mfasta", "fasta"))
 
-for i in range(len(records)):
-    seqRecordToCheck = str(records[i].seq)
-    seqRecordToCheckComplement = str(records[i].seq.complement())
+
+for x in range(500, 550):
+    seqRecordToCheck = str(records[x].seq)
+    seqRecordToCheckComplement = str(records[x].seq.complement())
 
     for seq in primerBindingSites:
-        primerSeq = str(seq.seq[::-1])
-        length = len(seq.seq)
+        primerSeq = str(seq.seq).split("(")
+        primerSeq = primerSeq[0][::-1]
 
-        if seqRecordToCheck[0:14] == primerSeq[0:14]:
-            if seqRecordToCheck[0:length] == primerSeq:
-                print "Record ID: " + records[i].id + " Primer Binding Sites: (complete) Match. " + "Primer ID: " + seq.id
-            else:
-                print "Record ID: " + records[i].id + " Primer Binding Sites: (partaial) Match. " + "Primer ID: " + seq.id
+        partialPrimerSeq = primerSeq[0:14]
 
-        if seqRecordToCheckComplement[0:14] == primerSeq[0:14]:
-            if seqRecordToCheckComplement[0:length] == primerSeq:
-                print "Record ID: " + records[i].id + " Primer Binding Sites: (complete) Match at complement. " + "Primer ID: " + seq.id
-            else:
-                print "Record ID: " + records[i].id + " Primer Binding Sites: (partaial) Match at complement. " + "Primer ID: " + seq.id
+
+        matchingPrimerPositions = SeqUtils.nt_search(seqRecordToCheck, partialPrimerSeq)
+
+        if (len(matchingPrimerPositions) > 1):
+            length = len(matchingPrimerPositions)
+            for j in range(1, length):
+
+                if primerSeq == seqRecordToCheck[matchingPrimerPositions[j] : matchingPrimerPositions[j] + len(primerSeq)]:
+                    print "Complete Match at position: " + str(matchingPrimerPositions[j]) + ".." + str(matchingPrimerPositions[j] + len(primerSeq)) + " in record: " + str(records[x].id)
+                else:
+                    print "Partial Match at position: " + str(matchingPrimerPositions[j]) + ".." + str(matchingPrimerPositions[j] + len(partialPrimerSeq)) + " in record: " + str(records[x].id)
+
+
+        matchingPrimerPositions = SeqUtils.nt_search(seqRecordToCheckComplement, partialPrimerSeq)
+
+        if (len(matchingPrimerPositions) > 1):
+            length = len(matchingPrimerPositions)
+            for j in range(1, length):
+
+                if primerSeq == seqRecordToCheckComplement[matchingPrimerPositions[j] : matchingPrimerPositions[j] + len(primerSeq)]:
+                    print "Complete Match on COMPLEMENT at position: " + str(matchingPrimerPositions[j]) + ".." + str(matchingPrimerPositions[j] + len(primerSeq)) + " in record: " + str(records[x].id)
+                else:
+                    print "Partial Match on COMPLEMENT at position: " + str(matchingPrimerPositions[j]) + ".." + str(matchingPrimerPositions[j] + len(partialPrimerSeq)) + " in record: " + str(records[x].id)
