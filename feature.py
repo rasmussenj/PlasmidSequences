258972209b6500df10690c02d384c5ee3e5b730f
Fabrizio Parrillo <fabriparrilllo@hotmail.com>
2015-12-14 16:13:42 +0100

    neu vergleichen einlesen

--- feature.py
+++ feature.py
@@ -1,5 +1,11 @@
 import math
-
+only_note = ['promoter', 'oriT', 'rep_origin', 'primer_bind', 'terminator', 'misc_signal', 'misc_recomb', 'LTR', 'enhancer']
+note_and_gene = ['-35_signal', '-10_signal', 'RBS', 'polyA_signal', 'sig_peptide']
+gene_and_product = ['CDS']
+note_and_bound_moiety = ['protein_bind', 'misc_binding']
+note_and_mobile = ['mobile_element']
+gene = ['mRNA']
+product = ['tRNA', 'rRNA']
 
 class FeatureStatistic:
     def __init__(self, name):
@@ -11,11 +17,80 @@ class FeatureStatistic:
             self.seq = seq
             self.count = count
             self.present_in_percent = None
-            self.product = None
-            self.gene = None
-            self.bound_moiety = None
-            self.mobile = None
-            self.note = None
+            self.product = []
+            self.gene = []
+            self.bound_moiety = []
+            self.mobile = []
+            self.note = []
+
+
+        def isSame(self, nextFeature):
+            if nextFeature.type in only_note:
+                if self.note[0] == nextFeature.note:
+                    self.gene.append()
+                    self.bound_moiety.append()
+                    self.mobile.append()
+                    self.product.append()
+
+                    return True
+
+            if nextFeature.type in note_and_gene:
+                if (self.note[0] == nextFeature.note and
+                    self.gene[0] == nextFeature.gene):
+
+                    self.bound_moiety.append(nextFeature.bound_moiety)
+                    self.mobile.append(nextFeature.mobile)
+                    self.product.append(nextFeature.product)
+                    return True
+
+
+            if nextFeature.type in gene_and_product:
+                if (self.gene[0] == nextFeature.gene and
+                            self.product[0] == nextFeature.product):
+
+                    self.bound_moiety.append(nextFeature.bound_moiety)
+                    self.mobile.append(nextFeature.mobile)
+                    self.note.append(nextFeature.note)
+
+                    return True
+
+            if nextFeature.type in note_and_bound_moiety:
+                if (self.note[0] == nextFeature.note and
+                            self.bound_moiety[0] == nextFeature.bound_moiety):
+                    self.gene.append(nextFeature.gene)
+
+                    self.mobile.append(nextFeature.mobile)
+
+                    self.product.append(nextFeature.product)
+                    return True
+
+            if nextFeature.type in note_and_mobile:
+                if (self.note[0] == nextFeature.note and
+                            self.mobile[0] == nextFeature.mobile):
+                    self.gene.append(nextFeature.gene)
+                    self.bound_moiety.append(nextFeature.bound_moiety)
+
+                    self.product.append(nextFeature.product)
+                    return True
+
+
+            if nextFeature.type in gene:
+                if (self.gene[0] == nextFeature.gene):
+
+                    self.bound_moiety.append(nextFeature.bound_moiety)
+                    self.mobile.append(nextFeature.mobile)
+                    self.note.append(nextFeature.note)
+                    self.product.append(nextFeature.product)
+                    return True
+
+            if nextFeature.type in product:
+                if (self.product[0] == nextFeature.product):
+                    self.gene.append(nextFeature.gene)
+                    self.bound_moiety.append(nextFeature.bound_moiety)
+                    self.mobile.append(nextFeature.mobile)
+                    self.note.append(nextFeature.note)
+                    return True
+
 
 class Feature:
     def __init__(self, seq, name, count):
@@ -74,27 +149,35 @@ class Statistic:
         for f in self.featureContainer:
             print("+---------------------------------------------------------")
             print("| %s " % f.name)
-            print("| %-20s %-20s %10s %50s %50s %50s"% ("count", "seq Start","percent", "note", "Gene", "Product"))
+            print("| %-20s %-20s %10s %50s %50s %50s %50s %50s"%
+                  ("count", "seq Start","percent", "note", "Gene", "Product",
+                   "bound_moiety", "mobile"))
 
             for variation_f in f.varationList:
-                print("|%-20d %-20s %10d %% %50s %50s %50s"% (variation_f.count, variation_f.seq[:10],
+                print("|%-20d %-20s %10d %% %50s %50s %50s %50s %50s"% (variation_f.count, variation_f.seq[:10],
                                                     variation_f.present_in_percent,
-                                                    variation_f.note, variation_f.gene, variation_f.product))
+                                                    variation_f.note, variation_f.gene,
+                                                                  variation_f.product, variation_f.bound_moiety,
+                                                                  variation_f.mobile))
 
         log_file = open("Log_Features_in_vector.txt", mode='w')
-        log_file.write(" %s \t %s \t %s \t %s  \t %s \t %s \t %s \n"% ("feature", "count", "seq Start","percent", "note", "Gene", "Product"))
+        log_file.write(" %s \t %s \t %s \t %s  \t %s \t %s \t %s \t %s \t %s \n"%
+                       ("feature", "count", "seq Start","percent", "note", "Gene",
+                        "Product", "bound_moiety", "mobile"))
         for f in self.featureContainer:
 
             log_file.write("\n\n %s \n" % f.name)
             for variation_f in f.varationList:
 
-                log_file.write("%s \t  %s \t %d %% \t %s \t %s \t %s \t %s \n"% (
+                log_file.write("%s \t  %s \t %d %% \t %s \t %s \t %s \t %s \t %s \t %s\n"% (
                     " ",
                     variation_f.count,
                     variation_f.present_in_percent,
                     variation_f.note,
                     variation_f.gene,
                     variation_f.product,
-                    variation_f.seq))
+                    variation_f.seq,
+                    variation_f.bound_moiety,
+                    variation_f.mobile))
 
         log_file.close()
--- load.py
+++ load.py
@@ -12,7 +12,7 @@ product = ['tRNA', 'rRNA']
 features_to_check_list = [only_note, note_and_gene, gene_and_product, note_and_bound_moiety, note_and_mobile, gene, product]
 
 def getFeature():
-    handle = open("vectors-100.gb", "rU")
+    handle = open("vectors.gb", "rU")
     for record in SeqIO.parse(handle, "genbank") :
         if len(record.seq) > 1500:
             for f in record.features:
@@ -72,22 +72,22 @@ def countFeatures(features):
                     features_Container.append(FeatureStatistic(f))
         features_Container
         # get the FeatureStatistic object which corresponds with the actual feature
-        for statFeature in it.ifilter(lambda f: f.name == feature.name, features_Container):
+        for statFeature in it.ifilter(lambda f: f.name == feature.type, features_Container):
             seq_in_list = False
 
             # search for the corresponding seq in statFeature
-            for variation in it.ifilter(lambda f: f.seq == feature.seq, statFeature.varationList):
+            for variation in it.ifilter(lambda v: v.isSame(feature), statFeature.varationList):
                 variation.count += 1
                 seq_in_list = True
 
             # if seq not found in statFeature, create a new variation of the feature
             if not seq_in_list:
                 new_variation = FeatureStatistic.Varation(feature.seq,1)
-                new_variation.note = feature.note
-                new_variation.mobile = feature.mobile
-                new_variation.bound_moiety = feature.bound_moiety
-                new_variation.gene = feature.gene
-                new_variation.product = feature.product
+                new_variation.note = {feature.note}
+                new_variation.mobile = {feature.mobile}
+                new_variation.bound_moiety = {feature.bound_moiety}
+                new_variation.gene = {feature.gene}
+                new_variation.product = {feature.product}
                 statFeature.varationList.append(new_variation)
 
     return features_Container
