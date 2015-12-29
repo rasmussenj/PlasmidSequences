99e751425a0f61d625539833b81c105161406f56
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2015-12-29 18:21:33 +0100

    es funktioniert
    wieder

--- BlastX.py
+++ BlastX.py
@@ -1,15 +1,1 @@
-from Bio import SeqIO
-from Bio.Blast import NCBIWWW
 
-
-#record = SeqIO.read("EcoliK12.gb", "genbank")
-records = list(SeqIO.parse("vectors.gb", "genbank"))
-seq = records[0].seq
-parser = NCBIWWW.BlastParser()
-
-qblast_output = NCBIWWW.qblast("blastx", "refseq_protein", seq)
-result = parser.parse(qblast_output)
-
-text = qblast_output.read()
-
-print text
